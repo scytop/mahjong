fdfa1864619e5fb2245f79db5b80ad31917c24ee
Jonathan Chu <scytop@gmail.com>
2013-02-12 10:24:28 -0800

    'I_forgot_this_one'

---
+++ globals_h.cpp
@@ -0,0 +1,10 @@
+//globals.h
+
+
+char const CIRCLE	= 'a';	// 36
+char const LINE 	= 'b';	// 36
+char const WAN		= 'c';	// 36
+char const WIND		= 'd';	// 28
+char const FLOWER 	= 'e';	//  8
+							//144
+
